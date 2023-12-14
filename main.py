437c9a6106cda9cbde11b18837a9f326cae07d2b
Skye <Skye@supermind.us>
2023-12-14 14:07:03 -0500

    changed chef, added orange moving across creen, added bomb moving across screen

--- main.py
+++ main.py
@@ -8,19 +8,30 @@ pygame.display.set_caption("Citrus Slicer!")
 FPS = 60
 VEL = 5
 
-CHEF_WIDTH, CHEF_HEIGHT = 80, 100
+CHEF_WIDTH, CHEF_HEIGHT = 70, 90
+BOMB_WIDTH, BOMB_HEIGHT = 50, 50
+ORANGE_WIDTH, ORANGE_HEIGHT = 35, 35
 
-CHEF_IMAGE = pygame.image.load(os.path.join('Assets','chef-gif.gif'))
+CHEF_IMAGE = pygame.image.load(os.path.join('Assets','Chef2.gif'))
 CHEF_SPRITE = pygame.transform.scale(CHEF_IMAGE, (CHEF_WIDTH, CHEF_HEIGHT))
 
-def draw_window(chef_position):
+BOMB_IMAGE = pygame.image.load(os.path.join('Assets','bomb.png'))
+BOMB_SPRITE = pygame.transform.scale(BOMB_IMAGE, (BOMB_WIDTH, BOMB_HEIGHT))
+
+ORANGE_IMAGE = pygame.image.load(os.path.join('Assets','orange.png'))
+ORANGE_SPRITE= pygame.transform.scale(ORANGE_IMAGE,(ORANGE_WIDTH, ORANGE_HEIGHT))
+
+def draw_window(chef_position, bomb_position, orange_position):
     WIN.fill((66, 245, 233))
     WIN.blit(CHEF_SPRITE, (chef_position.x, chef_position.y))
+    WIN.blit(BOMB_SPRITE,(bomb_position.x, bomb_position.y))
+    WIN.blit(ORANGE_SPRITE,(orange_position.x, orange_position.y) )
     pygame.display.update()
 
 def main():
+    bomb_position =pygame.Rect(900,200, BOMB_WIDTH, BOMB_HEIGHT)
     chef_position =pygame.Rect(100,200, CHEF_WIDTH, CHEF_HEIGHT)
-
+    orange_position = pygame.Rect(900, 250, ORANGE_WIDTH, ORANGE_HEIGHT)
 
 
     clock = pygame.time.Clock()
@@ -41,7 +52,10 @@ def main():
         if keys_pressed[pygame.K_s]: #down
             chef_position.y += VEL
 
-        draw_window(chef_position)
+        bomb_position.x -= 3
+        orange_position.x -= 3
+
+        draw_window(chef_position, bomb_position, orange_position)
 
     pygame.quit()
 
